00efcda260da4495c4051988cd4c5e03552e7e98
shwetasai <shwetasaini250796@gmail.com>
2024-06-19 12:47:49 +0530

    Your commit message here

--- comments/models.py
+++ comments/models.py
@@ -7,7 +7,14 @@ class Comment(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f'Comment on {self.post.id}'
+
+
 class Reply(models.Model):
     comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Reply to comment {self.comment.id}'
